f0da55db42d4406d309f514d51a3ac25250aff4e
chenxuan <1249418549@qq.com>
2017-03-17 12:50:13 +0800

    111111

---
+++ 前端组件/affix/js/index.js
@@ -0,0 +1,32 @@
+/*！
+ *XX
+ *date:2016-01-01
+ *author: chengang;
+ */
+$(function() {
+    // $(window).on('scroll', function() {
+            //     if ($(window).scrollTop() < 360) {
+            //         $("#myNav").hide();
+            //     } else {
+            //         $("#myNav").show();
+            //         $("#myNav").affix({
+            //             offset: {
+            //                 bottom: 243
+            //             }
+            //         });
+            //     }
+            // })
+$("#myNav").affix({
+    offset: {
+    	top :100,
+        bottom: 0
+    }
+});
+
+  //     $('#myNav').affix({
+  //   offset: {
+  //     top: 100
+  //   , bottom: 0
+  //   }
+  // })
+})
