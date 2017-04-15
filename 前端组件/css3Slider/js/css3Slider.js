e7c26112953b4e8b0f498e86508785c16d9ff7e1
chenxuan <1249418549@qq.com>
2017-04-15 10:57:15 +0800

    first commit

--- 前端组件/css3Slider/js/css3Slider.js
+++ 前端组件/css3Slider/js/css3Slider.js
@@ -103,7 +103,7 @@
             });
             // Pinch设置视角perspective
             new Hammer($father[0]).on("pinchin", function(ev) {
-               alert(222);
+               alert(1111);
             });
         }
 
